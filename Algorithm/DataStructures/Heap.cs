70c131f89f6ff4b645e515d5c23d63052d474107
Александр Семенов <korwin86@gmail.com>
2020-09-23 14:51:58 +0700

    изменена реализация пирамидальной сортировки(heap) и допилена визуализация под неё

--- Algorithm/DataStructures/Heap.cs
+++ Algorithm/DataStructures/Heap.cs
@@ -3,28 +3,26 @@ using System.Collections.Generic;
 
 namespace Algorithm.DataStructures
 {
-    class Heap<T> where T : IComparable
+    public class Heap<T> : AlgorithmBase<T> where T : IComparable
     {
-        private List<T> items = new List<T>();
-        public int Count => items.Count;
+        public int Count => Items.Count;
+        //private int sortedCount = 0;
 
         public T Peek()
         {
             if (Count > 0)
             {
-                return items[0];
+                return Items[0];
             }
             else
             {
-                throw new ArgumentNullException(nameof(items), "Куча пуста");
+                throw new ArgumentNullException(nameof(Items), "Куча пуста");
             }
         }
 
-        public Heap() { }
-
         public Heap(IEnumerable<T> items)
         {
-            this.items.AddRange(items);
+            Items.AddRange(items);
             for (int i = Count; i >= 0; i--)
             {
                 Sort(i);
@@ -33,14 +31,14 @@ namespace Algorithm.DataStructures
 
         public void Add(T item)
         {
-            items.Add(item);
+            Items.Add(item);
 
             var currentIndex = Count - 1;
             var parentIndex = GetParentIndex(currentIndex);
 
-            while (currentIndex > 0 && items[parentIndex].CompareTo(items[currentIndex]) == 1)
+            while (currentIndex > 0 && Compare(Items[parentIndex], Items[currentIndex]) == -1)
             {
-                Swap(currentIndex, parentIndex);
+                Swop(currentIndex, parentIndex);
 
                 currentIndex = parentIndex;
                 parentIndex = GetParentIndex(currentIndex);
@@ -49,30 +47,31 @@ namespace Algorithm.DataStructures
 
         public T GetMax()
         {
-            var result = items[0];
-            items[0] = items[Count - 1];
-            items.RemoveAt(Count - 1);
-            Sort(0);
+            var result = Items[0];
+            Items[0] = Items[Count - 1];
+            Items.RemoveAt(Count - 1);
+            Sort(0);            
             return result;
         }
 
-        private void Sort(int curentIndex)
+        private void Sort(int curentIndex, int maxLenght = -1)
         {
             int maxIndex = curentIndex;
             int leftIndex;
             int rightIndex;
+            maxLenght = maxLenght == -1 ? Count : maxLenght;
 
-            while (curentIndex < Count)
+            while (curentIndex < maxLenght)
             {
                 leftIndex = 2 * curentIndex + 1;
                 rightIndex = 2 * curentIndex + 2;
 
-                if (leftIndex < Count && items[leftIndex].CompareTo(items[maxIndex]) == -1)
+                if (leftIndex < maxLenght && Compare(Items[leftIndex], Items[maxIndex]) == 1)
                 {
                     maxIndex = leftIndex;
                 }
 
-                if (rightIndex < Count && items[rightIndex].CompareTo(items[maxIndex]) == -1)
+                if (rightIndex < maxLenght && Compare(Items[rightIndex], Items[maxIndex]) == 1)
                 {
                     maxIndex = rightIndex;
                 }
@@ -82,16 +81,9 @@ namespace Algorithm.DataStructures
                     break;
                 }
 
-                Swap(curentIndex, maxIndex);
+                Swop(curentIndex, maxIndex);
                 curentIndex = maxIndex;
             }
-        }
-
-        private void Swap(int currentIndex, int parentIndex)
-        {
-            var temp = items[currentIndex];
-            items[currentIndex] = items[parentIndex];
-            items[parentIndex] = temp;
         }
 
         private int GetParentIndex(int currentIndex)
@@ -99,14 +91,13 @@ namespace Algorithm.DataStructures
             return (currentIndex - 1) / 2;
         }
 
-        public List<T> Order()
+        protected override void MakeSort()
         {
-            var result = new List<T>();
-            while (Count > 0)
+            for (int i = Count - 1; i >= 0; i--)
             {
-                result.Add(GetMax());
+                Swop(0, i);
+                Sort(0, i);
             }
-            return result;
         }
     }
 }
--- Algorithm/HeapSort.cs
+++ Algorithm/HeapSort.cs
@@ -1,15 +1,22 @@
 ﻿using Algorithm.DataStructures;
 using System;
+using System.Collections.Generic;
 
 namespace Algorithm
 {
-    public class HeapSort<T> : AlgorithmBase<T> where T : IComparable
-    {
-        protected override void MakeSort()
-        {
-            var heap = new Heap<T>(Items);
-            var sorted = heap.Order();
-            Items = sorted;
-        }
-    }
+    //public class HeapSort<T> : Heap<T> where T : IComparable
+    //{
+        //public HeapSort(IEnumerable<T> items) : base(items) { }
+        //public HeapSort() { }
+
+        //protected override void MakeSort()
+        //{
+        //    var heap = new Heap<T>(Items);
+
+
+
+        //    var sorted = heap.Order();
+        //    Items = sorted;
+        //}
+    //}
 }
