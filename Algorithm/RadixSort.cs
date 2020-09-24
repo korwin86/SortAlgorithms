3f78b93a9dc9cebcf7fd290e09acf97d3fd1907d
Александр Семенов <korwin86@gmail.com>
2020-09-24 16:28:38 +0700

    Поразрядная сортировка LSD (Radix sort)

---
+++ Algorithm/RadixSort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm
+{
+    public class RadixSort<T> : AlgorithmBase<T> where T : IComparable
+    {
+        public RadixSort(IEnumerable<T> items) : base(items) { }
+        public RadixSort() { }
+
+        protected override void MakeSort()
+        {
+            var groups = new List<List<T>>();
+            for (int i = 0; i < 10; i++)
+            {
+                groups.Add(new List<T>());
+            }
+
+            int length = GetMaxLength();
+
+            for (int step = 0; step < length; step++)
+            {
+                // Распределение элементов по корзинам
+                foreach (var item in Items)
+                {
+                    var i = item.GetHashCode();
+                    var value = i % (int)Math.Pow(10, step + 1) / (int)Math.Pow(10, step);
+                    groups[value].Add(item);
+                }
+
+                Items.Clear();
+                // Сборка элементов
+                foreach (var group in groups)
+                {
+                    foreach (var item in group)
+                    {
+                        Items.Add(item);
+                    }
+                }
+                // Очистка корзин
+                foreach (var group in groups)
+                {
+                    group.Clear();
+                }
+            }
+
+        }
+
+        private int GetMaxLength()
+        {
+            var length = 0;
+            foreach (var item in Items)
+            {
+                if (item.GetHashCode() < 0)
+                {
+                    throw new ArgumentException("Поразрядная сортировка поддерживает только целые числа (больше либо равно нуля)", nameof(Items));
+                }
+
+                // Не работает со значением item = 0, Даёт -inf
+                //var l = Convert.ToInt32(Math.Log10(item.GetHashCode()) + 1);
+                var l = item.GetHashCode().ToString().Length;
+                if (l > length)
+                {
+                    length = l;
+                }
+            }
+            return length;
+        }
+    }
+}
