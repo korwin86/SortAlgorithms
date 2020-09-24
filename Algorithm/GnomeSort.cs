ebba202a02c45f179bf8c60c14942727276d32a6
Александр Семенов <korwin86@gmail.com>
2020-09-24 11:05:30 +0700

    решение бага от CODE BLOG

--- Algorithm/GnomeSort.cs
+++ Algorithm/GnomeSort.cs
@@ -13,7 +13,7 @@ namespace Algorithm
             var i = 1;
             while (i < Items.Count)
             {
-                if (i == 0 || Compare(Items[i], Items[i - 1]) == 1 || Compare(Items[i], Items[i - 1]) == 0)
+                if (i == 0 || Compare(Items[i], Items[i - 1]) != -1)
                 {
                     i++;
                 }
